f6d79e711f6b76e961a6a84f4a1ff93e624acb55
Kevin Rincon <kevinrincon8@gmail.com>
2023-09-29 00:54:45 -0500

    Punto de partida del proyecto

---
+++ src/database/core.py
@@ -0,0 +1,22 @@
+from fastapi import Depends
+from src import config
+from typing import Annotated
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from starlette.requests import Request
+
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db(request: Request):
+    return request.state.db
+
+
+DbSession = Annotated[Session, Depends(get_db)]
+
+
---
+++ src/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+from .database.core import engine
+from starlette.requests import Request
+
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+# ASGI para el framework
+app = FastAPI()
+
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    try:
+        # Crea una sesión de base de datos
+        session = scoped_session(sessionmaker(bind=engine))
+        request.state.db = session()
+        # Llama al siguiente manipulador en la cadena
+        response = await call_next(request)
+    except Exception as e:
+        raise e from None
+    finally:
+        # Cierra la sesión de la base de datos
+        request.state.db.close()
+
+    return response
+
+
+@app.get("/")
+def hello_world():
+    return "Hola Mundo"
+
+
+@app.get("/test-db-connection")
+def test_db_connection():
+    try:
+        # Realiza una consulta de prueba en la base de datos
+        result = engine.execute("SELECT 1")
+        return {"message": "Conexión a la base de datos exitosa"}
+    except Exception as e:
+        return {"message": "Error al conectar a la base de datos", "error": str(e)}

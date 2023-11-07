2e50e96dc54804105c309a2b668cf48fb55703be
Kevin Rincon <kevinrincon8@gmail.com>
2023-11-07 06:39:35 -0500

    Se añade el requisito de autentificacion y permisos a la ruta de usuario

--- src/api.py
+++ src/api.py
@@ -1,35 +1,19 @@
 from starlette.responses import JSONResponse
-from pydantic import BaseModel
-from typing import Optional, List
 
 from fastapi import APIRouter
 
 from src.programa_academico.router import router as programa_academico_router
 from src.funcion_sustantiva.router import router as funcion_sustantiva_router
 from src.usuario.router import router as usuario_router
+from src.actividad.router import router as actividad_router
+from src.dependencia.router import router as dependencia_router
 
-
-class ErrorMessage(BaseModel):
-    msg: str
-
-
-class ErrorResponse(BaseModel):
-    detail: Optional[List[ErrorMessage]]
-
-
-# Para rutas sin autenticar
-authenticated_api_router = APIRouter()
+from src.auth.dependencies import PermissionsValidator, validate_token
+from fastapi import Depends
 
 
 api_router = APIRouter(
     default_response_class=JSONResponse,
-    responses={
-        400: {"model": ErrorResponse},
-        401: {"model": ErrorResponse},
-        403: {"model": ErrorResponse},
-        404: {"model": ErrorResponse},
-        500: {"model": ErrorResponse},
-    },
 )
 
 
@@ -47,9 +31,29 @@ api_router.include_router(
     tags=["Funciones Sustantivas"],
 )
 
+# Proteger autenticacion
+# @app.get("/api/messages/protected", dependencies=[Depends(validate_token)])
+# def protected():
+#     return {"text": "This is a protected message."}
+
 
 api_router.include_router(
     usuario_router,
     prefix="/usuario",
     tags=["Usuarios"],
+    dependencies=[Depends(PermissionsValidator(["read:usuarios"]))],
+)
+
+
+api_router.include_router(
+    actividad_router,
+    prefix="/actividad",
+    tags=["Actividades"],
+)
+
+
+api_router.include_router(
+    dependencia_router,
+    prefix="/dependencia",
+    tags=["Dependencias"],
 )
